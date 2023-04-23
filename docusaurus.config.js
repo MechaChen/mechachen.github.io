72f9f1ab83691c116b607d3621451f676a12dcf6
MechaChen <tumlivein@gmail.com>
2023-04-23 22:51:46 +0800

    chore: add showLastUpdateDate option

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,6 +39,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
